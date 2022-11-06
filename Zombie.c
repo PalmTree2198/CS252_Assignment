d84f8f1dbe01883aac803ce21f11fa14ab989af0
PalmTree2198 <76690892+PalmTree2198@users.noreply.github.com>
2022-11-06 22:55:32 +0530

    Update Zombie.c

--- Zombie.c
+++ Zombie.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
 
+int main (void) { 
+	pid_t pid; // Data type that represents the process ID 
+	pid = fork(); // To create a new process
+
+	if (pid < 0) { // Check if the child process has been created
+		fprintf(stdout, "Fork failed\n"); // Unable to create a child process
+		return 1;
+	}
+	else if (pid == 0) { // Successfully created a child process
+		printf("The process created is the child process.\n");
+	}
+	else if (pid > 0) { 
+		sleep(30); // Puts the parent process to sleep for 10 seconds.
+	}
+	return 0;	
+}
