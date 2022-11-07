acc81c4b79aee39e07f2d6966bb65f3eb2aee7d7
PalmTree2198 <76690892+PalmTree2198@users.noreply.github.com>
2022-11-07 13:17:31 +0530

    Update Collatz_Algorithm.c

--- Collatz_Algorithm.c
+++ Collatz_Algorithm.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
 
 int main (int argc, char *argv[]) { // argc -> count of arguments, argv -> array of character pointers listing all the arguments
 	int n; // Input integer 
